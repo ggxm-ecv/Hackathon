8623daa74220f6cde83f83ff70806a8e238e8643
Gautier <hello.gautiermaire@gmail.com>
2021-09-21 17:02:22 +0200

    debut transition style

--- js/script-404.js
+++ js/script-404.js
@@ -1,24 +1,59 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
-var counter=0;
-console.log('test');
-function jump(){
-    if(character.classList == "animate"){return}
-    character.classList.add("animate");
-    setTimeout(function(){
-        character.classList.remove("animate");
-    },800);
-}
-var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 3s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
-    }
-}, 30);
+(function () {
+
+	const buttonStart = document.getElementById('game-start');
+	const character = document.getElementById('game-character');
+	const block = document.getElementById('game-block');
+	const score = document.getElementById('game-score');
+
+	var counter = 0;
+
+	function gameStart() {
+
+		// Game Init
+
+
+
+		// Game Process
+		let gameProcess = setTimeout(function(){
+
+			document.addEventListener('click', function () {
+				jump();
+			});
+	
+			var checkDead = setInterval(function () {
+				let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+				let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+				if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
+					block.style.animation = 'none';
+					alert('Game Over. score: ' + Math.floor(counter / 100));
+					counter = 0;
+					block.style.animation = 'block 3s infinite linear';
+				} else {
+					counter++;
+					score.innerHTML = Math.floor(counter / 100);
+				}
+			}, 30);
+
+		}, 400);
+		
+	}
+
+	function jump() {
+		if (character.classList == 'animate') { return }
+		character.classList.add('animate');
+		setTimeout(function () {
+			character.classList.remove('animate');
+		}, 800);
+	}
+
+
+	// Start Game
+	buttonStart.addEventListener('click', function () {
+		document.body.classList.add('game-strated');
+		gameStart();
+	});
+
+
+})();
+
+
